ad3ffe4292bdd7871d84a87ac703df1c338d3f16
Lory <lorymichelle.brescianimiristice@uniroma1.it>
2022-06-22 12:22:49 +0200

    first commit

---
+++ column-one-below/FileImporter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace column_one_below
+{
+    public class FileImporter
+    {
+        private string FilePath { set; get; }
+        private Dictionary<string, int>? Headers { set; get; }
+        public string HeaderMap { get => GetHeadersMap(); }
+        public FileImporter(string filePath)
+        {
+            FilePath = filePath;
+            Headers = ImportHeaders(filePath);
+        }
+
+        private Dictionary<string, int>? ImportHeaders(string filePath)
+        {
+            using var fileStream = File.OpenRead(filePath);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+            var counter = 0;
+            var line = streamReader.ReadLine();
+            var headersArray = line.Split(';');
+            var headers = Enumerable.Range(0, headersArray.Length).ToDictionary(x => headersArray[x]);
+            return headers;
+        }
+
+        public string[] MergeValues(string headersIndeces)
+        {
+            var indeces = Array.Empty<int>();    
+            if (headersIndeces.Contains("-"))
+            {
+                var limit = headersIndeces.Split('-').ToArray();
+                indeces = Enumerable.Range(int.Parse(limit[0]), int.Parse(limit[1]) - int.Parse(limit[0])+1).ToArray();
+            }
+            else
+            {
+                indeces = headersIndeces.Split(',').Select(index => int.Parse(index)).ToArray();
+            }
+
+            var columnValues = new List<string>[indeces.Length];
+            for (int i = 0; i < indeces.Length; i++)
+            {
+                columnValues[i] = new List<string>();
+            }
+
+            using var fileStream = File.OpenRead(FilePath);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+            var line = streamReader.ReadLine();
+            while (true)
+            {
+                line = streamReader.ReadLine();
+                if (string.IsNullOrEmpty(line)) break;                
+                var fields = line.Split(';').ToArray();
+
+                for (int i = 0; i< columnValues.Length; i++)
+                {
+                    columnValues[i].Add(fields[indeces[i]-1]);
+                }
+            }
+            var newColumn = new List<string>();
+            for (int i = 0; i < columnValues.Length; i++)
+            {
+                newColumn.AddRange(columnValues[i]);
+            }
+            return newColumn.ToArray();
+        }
+
+        private string GetHeadersMap()
+        {
+            var strings = new List<string>();
+            foreach (var header in Headers)
+            {
+                strings.Add($"{header.Value + 1}: [{header.Key}]");
+            }  
+            return string.Join("; ", strings);    
+        }
+
+    }
+}
---
+++ column-one-below/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using column_one_below;
+
+Console.Write("Insert path of the file to elaborate: ");
+var filePath = Console.ReadLine();
+Console.WriteLine();
+if (filePath is null) return;
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"File [{filePath}] not found");
+    
+    return;
+}
+
+var fileImporter = new FileImporter(filePath);
+Console.WriteLine($"Headers are: {fileImporter.HeaderMap}");
+Console.WriteLine("Insert column indeces to merge, separated by ',' or column range separated by '-': ");
+var columnIndeces = Console.ReadLine();
+Console.WriteLine();
+if (columnIndeces is null) return;
+
+var newColumn = fileImporter.MergeValues(columnIndeces);
+
+Console.Write("Insert path of the output file: ");
+var outputFilePath = Console.ReadLine();
+Console.WriteLine();
+if (outputFilePath is null) return;
+if (File.Exists(outputFilePath)) File.Delete(outputFilePath);
+File.WriteAllLines(outputFilePath, newColumn);
+Console.WriteLine($"Column exported to {outputFilePath}");
